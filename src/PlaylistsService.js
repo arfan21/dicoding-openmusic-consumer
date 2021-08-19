ce58f2702656acf39dc00af46600541588cf37fe
arfan21 <arfan.jadul10@gmail.com>
2021-08-19 17:33:15 +0700

    finisih consumer

---
+++ src/PlaylistsService.js
@@ -0,0 +1,19 @@
+const { Pool } = require('pg');
+
+class PlaylistsService {
+    constructor() {
+        this._pool = new Pool();
+    }
+
+    async getPlaylistsByOwner(playlistId) {
+        const query = {
+            text: 'SELECT songs.id, songs.title, songs.performer FROM playlists INNER JOIN playlistsongs ON playlistsongs.playlist_id = playlists.id INNER JOIN songs ON playlistsongs.song_id = songs.id WHERE playlists.id = $1',
+            values: [playlistId],
+        };
+
+        const result = await this._pool.query(query);
+        return result.rows;
+    }
+}
+
+module.exports = PlaylistsService;
